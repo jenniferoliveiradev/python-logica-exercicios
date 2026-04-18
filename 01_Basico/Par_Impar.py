8c318d952d138f6ea1631f26a62ca3f5124398be
Aluno <aluno@matriculas.com>
2026-04-18 14:07:00 -0300

    Adicionar exercícios e estrutura de pastas

---
+++ 01_Basico/Par_Impar.py
@@ -0,0 +1,6 @@
+print("Calcule se é par ou ímpar:")
+numero = int(input("Digite um número: "))
+if numero % 2 == 0:
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é ímpar.")    
