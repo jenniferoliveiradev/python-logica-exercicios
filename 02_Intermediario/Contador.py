f6ed8a725d7deb0898f35f8c326c56a405b9819a
Jenni Oliveira <jennioliveiraluc@gmail.com>
2026-04-20 12:35:40 -0300

    Add files via upload

---
+++ 02_Intermediario/Contador.py
@@ -0,0 +1,5 @@
+i = 1
+limite = int(input("Digite um número limite: "))
+while i <= limite:
+   print(i)
+   i += 1
---
+++ 02_Intermediario/Tabuada.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um número: "))
+print(f"Tabuada de {numero}:")
+for i in range(1, 11):
+    print(f"{numero} x {i} = {numero * i}")
